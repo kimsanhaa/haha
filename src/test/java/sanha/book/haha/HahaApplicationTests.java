701068b00d3d0222e65f9852037d746f9e44b21a
kimsanhaa <ttksgk@gmail.com>
2022-06-19 16:42:34 +0900

    Initial commit

---
+++ src/test/java/sanha/book/haha/HahaApplicationTests.java
@@ -0,0 +1,13 @@
+package sanha.book.haha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
